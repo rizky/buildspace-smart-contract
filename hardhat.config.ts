37b4c85ccff5c89f44f867886a1bf480a9bef04c
ario@riz.ky <rizkyario@me.com>
2022-10-13 21:32:41 +0200

    Add First smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
