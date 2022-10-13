90a72b3237efc25ade9cce1610515fbae799da73
ario@riz.ky <rizkyario@me.com>
2022-10-13 21:48:19 +0200

    Improve smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,24 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address[] addresses;
+
     constructor() {
-        console.log("Bruv, I am a contract and I am smart");
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        addresses.push(msg.sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        for(uint256 i=0; i < addresses.length; i++) {
+          console.log("- ", addresses[i]);
+      }
+        return totalWaves;
     }
 }
